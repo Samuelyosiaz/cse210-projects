fd5b0d64bd84e06862e1010d4420f7e67c88e550
Samuelyosiaz <samilyosiaz@gmail.com>
2025-09-06 21:48:47 -0600

    Finished exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+
+        int countGuesses = 0;
+
+        int guessNumber = 0;
+
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessNumber = int.Parse(guess);
+
+            if (number > guessNumber)
+            {
+                Console.WriteLine("Higher");
+                countGuesses += 1;
+            }
+            else if (guessNumber > number)
+            {
+                Console.WriteLine("Lower");
+                countGuesses += 1;
+            }
+        } while (number != guessNumber);
+
+        Console.WriteLine($"You guessed it!, it took you {countGuesses} tries");
+
     }
 }
